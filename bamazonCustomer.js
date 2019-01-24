3888d38b3f7fc84ca5d4c9ea83a0d0c2967ab000
Mtnman <billman.designs@gmail.com>
2019-01-24 17:03:19 -0500

    ***

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,9 +1,10 @@
-//require mysql and inquirer modules
-
+//require modules
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+var itemID = [];
+var items = [];
 
-//Establish connections to bamazon_DB
+//establish connections to bamazon_DB
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -12,17 +13,50 @@ var connection = mysql.createConnection({
     database: "bamazon_DB"
 })
 
+//actions on connection
 connection.connect(function(err){
     if (err) throw err;
-
+    console.log("connected as id " + connection.threadId);
+    displayItems();
+    console.log("\n====================\n");
+    promptUser();
+    connection.end();
 })
 
-var Buyer = function({
-    this.promptUser = function(){
-        inquirer.prompt([
-            type: ,
-            message: ,
-            name:
-        ])
-    };
-})
+function displayItems(){
+    connection.query("SELECT * FROM products", function(err, res){
+        //console.log(res);
+        if (err) throw err;
+        for(let i = 0; i < res.length; i++){
+            itemID = res[i].item_id;
+            //console.log("this is itemID" + itemID);
+            //console.log("ID [" + res[i].item_id + "] " + res[i].product_name);
+            items.push("ID [" + res[i].item_id + "] " + res[i].product_name);
+        }
+    })
+}
+
+function promptUser(){
+    inquirer.prompt([
+        {
+        type: "list",
+        name: "id",
+        message: "Enter the [ID] of the product they would like to buy.",
+        choices: [items]
+        },
+        {
+            type: "number",
+            message: "The second message should ask how many units of the product they would like to buy.",
+            name: "qty"
+        }
+    ])
+    .then(function(res){
+        var a = res.id;
+        switch(a){
+            case "":
+            break;
+            default:
+            console.log("something went wrong");
+        }
+    })
+}
